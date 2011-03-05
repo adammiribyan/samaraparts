2fe38da8490f526de0f0fca3d1540926a95df76a
Adam <adam.miribyan@gmail.com>
2011-03-05 17:23:12 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+
+gem 'sqlite3'
+
+gem 'haml'
+gem 'haml-rails'
+gem 'less'
+gem 'simple_form'
+gem 'russian'
+
+gem 'jquery-rails', '~>0.2'
+
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samaraparts::Application.config.secret_token = '61438f0ed2a2e31c1d7ac783c22d954e5798e5c17025b257128190650e4f3861ee114899a275f084e4a7d2d59da43cd910501ee6c56905277ff7a8a18ed53d64'
