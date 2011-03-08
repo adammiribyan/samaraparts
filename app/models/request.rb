033b6d981944df4d4c8b9808d314c0d4003fdd3a
Adam <adam.miribyan@gmail.com>
2011-03-08 16:19:19 +0300

    facebox css

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,13 @@
 class Request < ActiveRecord::Base
 	attr_accessible :mark, :car_model, :year, :displacement, :vin, :engine_model, :required_part, :client_name, :client_phone
+	
+	validates :mark, 					:presence => true
+	validates :car_model, 		:presence => true
+	# validates :year, 					:presence => true
+	# validates :displacement, 	:presence => true
+	validates :vin, 					:presence => true
+	# validates :engine_model, 	:presence => true
+	validates :required_part, :presence => true
+	validates :client_name, 	:presence => true
+	validates :client_phone, 	:presence => true
 end
