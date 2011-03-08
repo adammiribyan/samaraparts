86d29272c6a1e5444c8d7dfebe0dee53661f4d1a
Adam <adam.miribyan@gmail.com>
2011-03-08 16:24:35 +0300

    temporarely deleted feedbacks from master branch

--- app/models/feedback.rb
+++
@@ -1,8 +0,0 @@
-class Feedback < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
-  
-  validates :full_name, :presence => true
-  validates :title, :presence => true
-  validates :body, :presence => true  
-  
-end
