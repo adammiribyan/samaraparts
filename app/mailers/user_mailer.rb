d9042a004f7f31e0894638b8feeacad6b1f7dbb1
Adam Miribyan <kreditmarket.com@gmail.com>
2011-03-15 13:48:24 +0300

    requests email changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,8 @@ class UserMailer < ActionMailer::Base
 	
   def part_request(request)
   	@request = request
-  	mail(:to => "adam.miribyan@gmail.com", :subject => "Новая онлайн заявка")
+#   mail(:to => "adam.miribyan@gmail.com", :subject => "Новая онлайн заявка")
+    mail(:to => "info@samaraparts.ru", :subject => "Новая онлайн заявка")
   end
   
 end
